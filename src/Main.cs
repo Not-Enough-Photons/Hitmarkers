2d7e2709046f65a7aec1c18e284ca29d0b0643b7
adamdev <hmullinsk@hotmail.com>
2023-04-21 14:17:05 -0400

    Updated version number

--- src/Main.cs
+++ src/Main.cs
@@ -17,7 +17,7 @@ namespace NEP.Hitmarkers
         public const string Description = "Simple hitmarkers mod for BONELAB."; // Description for the Mod.  (Set as null if none)
         public const string Author = "Not Enough Photons"; // Author of the Mod.  (MUST BE SET)
         public const string Company = "Not Enough Photons"; // Company that made the Mod.  (Set as null if none)
-        public const string Version = "2.1.0"; // Version of the Mod.  (MUST BE SET)
+        public const string Version = "2.7.5"; // Version of the Mod.  (MUST BE SET)
         public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
     }
 
